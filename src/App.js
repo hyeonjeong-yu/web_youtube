a4f2b3940027055d9da4ac4a34b934b013688542
hj <yhj960229@gmail.com>
2024-01-19 10:10:20 +0900

    Revert "페이지 구성"
    This reverts commit 29954ece51fc7210aa0ad8d7776af0d96181980a.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,8 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-import Home from './pages/Home';
-import Today from './pages/Today';
-import Developer from './pages/Developer';
-import Website from './pages/Website';
-import Youtube from './pages/Youtube';
-import Channel from './pages/Channel';
-import Video from './pages/Video';
-import Search from './pages/Search';
-import Not from './pages/Not';
+import React from 'react'
 
 const App = () => {
   return (
-    
-    // 해당 Components를 불러올 수 있는 Page setting
-    <BrowserRouter>
-        <Routes>
-            <Route path='/' element={ <Home/> }/>
-            <Route path='/today' element={ <Today/> } />
-            <Route path='/developer' element={ <Developer/> } />
-            <Route path='/website' element={ <Website/> } />
-            <Route path='/youtube' element={ <Youtube/> } />
-            <Route path='/channel/:channelID' element={ <Channel/> } />
-            <Route path='/video/:videoID' element={ <Video/> } />
-            <Route path='/search/:searchID' element={ <Search/> } />
-            <Route path='/*' element={ <Not/> } />
-            {/* 그 외 라우터 접속시 에러 페이지 출력 */}
-        </Routes>
-    </BrowserRouter>
+    <div>App</div>
   )
 }
 
