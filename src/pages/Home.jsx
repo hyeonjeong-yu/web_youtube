ce366c681cecc98791feaa91c5e086e2714c56d6
hj <yhj960229@gmail.com>
2024-01-22 19:20:16 +0900

    비디오 컴포넌트 통합 작업

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import Main from '../components/section/Main'
-import Today from '../components/contents/Today'
-import Developer from '../components/contents/Developer'
-import Website from '../components/contents/Website'
-import Port from '../components/contents/Port'
-import Youtube from '../components/contents/Youtube'
+import React from 'react';
+import Main from '../components/section/Main';
+import Today from '../components/contents/Today';
+import Developer from '../components/contents/Developer';
+import VideoSlider from '../components/videos/VideoSlider';
+
+// 비디오 컴포넌트 공통요소 통합 작업
+import { websiteText } from '../data/website';
+import { portfolioText } from '../data/portfolio';
+import { youtubeText } from '../data/youtube';
 
 const Home = props => {
     return (
@@ -14,10 +17,10 @@ const Home = props => {
             {/* Home(main)에서 보여줄 contents 컴포넌트 출력  */}
             <Today />
             <Developer />
-            <Website />
-            <Port />
-            <Youtube />
-            
+            <VideoSlider videos={websiteText} title='웹표준 사이트 만들기 기초 다지기' id='website' />
+            <VideoSlider videos={portfolioText} title='webstoryboy의 포트폴리오 방법 공유 리스트입니다.' id='portfolio' />
+            <VideoSlider videos={youtubeText} title='영상을 보고 코딩 공부하기' id='youtube' />
+
         </Main>
     )
 }
