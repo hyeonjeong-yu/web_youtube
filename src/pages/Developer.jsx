dcdf5e2db81388439df2b20728a22448f13ec3ea
hj <yhj960229@gmail.com>
2024-01-22 17:46:44 +0900

    추천 개발자 페이지 작업

--- src/pages/Developer.jsx
+++ src/pages/Developer.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
 import Main from '../components/section/Main';
+import { developerText } from '../data/developer';
+import { Link } from 'react-router-dom';
 
 const Developer = props => {
     return (
         <Main title='추천 개발자' description='추천 개발자 페이지입니다.'>
-            Developer
+            <section id='developerPage'>
+            <h2>추천 개발자를 소개합니다.</h2>
+            {
+                <div className='developer__inner'>
+                    {
+                        developerText.map((developer, key) => {
+                            return (
+                                <div className='developer' key={key}>
+                                    <div className='developer__img play__icon'>
+                                        <Link to={`/channel/${developer.channelId}`}>
+                                            <img src={developer.img} alt={developer.name} />
+                                        </Link>
+                                    </div>
+                                    <div className='developer__info'>
+                                        <Link to={`/channel/${developer.channelId}`}>
+                                            {developer.name}
+                                        </Link>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            }
+            </section>
         </Main>
     )
 }
