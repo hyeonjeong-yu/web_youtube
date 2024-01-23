d3fda451fe77c4406d88bd5d436fba4661c9d4d5
hj <yhj960229@gmail.com>
2024-01-23 19:06:40 +0900

    채널 상세 페이지 불러오기 작업

--- src/pages/Channel.jsx
+++ src/pages/Channel.jsx
@@ -1,12 +1,54 @@
-import React from 'react'
+// channelID 값을 가져오면 해당 채널을 보여줌
+
+import React, { useState, useEffect } from 'react'
 import Main from '../components/section/Main';
+import { useParams } from 'react-router-dom';
+
+import { CiBadgeDollar } from "react-icons/ci";
+import { CiMedal } from "react-icons/ci";
+import { CiRead } from "react-icons/ci";
 
 const Channel = props => {
-  return (
-    <Main title='유튜브 채널' description='유튜브 채널 페이지입니다.'>
-        Channel
-    </Main>
-  )
+
+    const { channelID } = useParams();
+    const [ channelDetail, setVideoDetail ] = useState();
+
+    useEffect(() => {
+        fetch(
+            `https://youtube.googleapis.com/youtube/v3/channels?part=snippet,statistics&id=${channelID}&type=video&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`,
+        )
+        .then(response => response.json())
+        .then(result => {
+            console.log(result);
+            setVideoDetail(result.items[0]);
+        })
+        .catch(error => console.log(error));
+
+    }, [channelID])
+
+    return (
+        <Main title='유튜브 채널' description='유튜브 채널 페이지입니다.'>
+            {
+                channelDetail && (
+                    <section id='channel'>
+                        <div className='channel__header'>
+                        <div className='circle'>
+                            <img src={channelDetail.snippet.thumbnails.high.url} alt={channelDetail.snippet.title} />
+                        </div>
+                        </div>
+                        <div className='channel__info'>
+                            <h3 className='title'>{channelDetail.snippet.title}</h3>
+                            <p className='desc'>{channelDetail.snippet.description}</p>
+                            <div className='info'>
+                                <span><CiBadgeDollar />{channelDetail.statistics.subscriberCount}</span>
+                                <span><CiMedal />{channelDetail.statistics.videoCount}</span>
+                                <span><CiRead />{channelDetail.statistics.viewCount}</span>
+                            </div>
+                        </div>
+                    </section>
+            )}
+        </Main>
+    )
 }
 
 Channel.propTypes = {}
